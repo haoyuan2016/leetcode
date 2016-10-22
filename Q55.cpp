d6a3349f0efaca45f8681276d87cf8723d6f630d
haoyuan2016 <haoyuan265@gmail.com>
2016-10-22 16:27:21 -0700

    Update Q55.cpp

--- Q55.cpp
+++ Q55.cpp
@@ -1,3 +1,4 @@
+// DP solution
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
@@ -11,6 +12,24 @@ public:
                 return false;
             dp[i] = max(dp[i - 1] - 1, nums[i]);
             if(i + dp[i] >= nums.size() - 1)
+                return true;
+        }
+        return false;
+    }
+};
+// Actually we do not need dp[i], simply one int value jump is enough
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.empty() || nums.size() == 1) return true;
+        int n = nums.size();
+        int jump = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {   
+            jump = max(jump - 1, nums[i]);
+            if(jump < 1) 
+                return false;
+            if(i + jump >= nums.size() - 1)
                 return true;
         }
         return false;
