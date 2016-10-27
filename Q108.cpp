8f917788efcaf3694e7ae05a4e3edee9d70e5463
haoyuan2016 <haoyuan265@gmail.com>
2016-10-27 01:51:37 -0700

    Update Q108.cpp

--- Q108.cpp
+++ Q108.cpp
@@ -23,3 +23,20 @@ public:
         return root;
     }
 };
+// Solution two
+// Note that learn to construct a vector from an existing vector. Note that newVec(oldVec.begin(), oldVec.end());
+// Two things, the first is oldVec.end() must be added, the second is oldVec.end() is not included.
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int> nums) {
+        if(nums.empty()) return NULL;
+        int n = nums.size();
+        int index = n / 2;
+        TreeNode* root = new TreeNode(nums[index]);
+        vector<int> leftVector(nums.begin(), nums.begin() + index);
+        vector<int> rightVector(nums.begin() + index + 1, nums.end());
+        root->left = sortedArrayToBST(leftVector);
+        root->right = sortedArrayToBST(rightVector);
+        return root;
+    }
+};
