c7d524104afc3d0aba36afc16d46f8cfd0bf6c8d
haoyuan2016 <haoyuan265@gmail.com>
2016-10-31 16:52:09 -0700

    Update Q118.cpp

--- Q118.cpp
+++ Q118.cpp
@@ -3,11 +3,9 @@ public:
     vector<vector<int>> generate(int numRows) {
         if(numRows == 0) return vector<vector<int>>{};
         if(numRows == 1) return vector<vector<int>>{vector<int>{1}};
-        if(numRows == 2) return vector<vector<int>>{vector<int>{1}, vector<int>{1,1}};
         vector<vector<int>> res;
         res.push_back(vector<int>{1});
-        res.push_back(vector<int>{1,1});
-        int cur = 3;
+        int cur = 2;
         while(cur <= numRows)
         {
             vector<int> tmp(cur, 1);
