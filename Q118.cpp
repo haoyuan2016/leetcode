e84d61873b8d98f4960f2bf95b40164db04cdfe6
haoyuan2016 <haoyuan265@gmail.com>
2016-10-31 16:50:35 -0700

    Create Q118.cpp

---
+++ Q118.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        if(numRows == 0) return vector<vector<int>>{};
+        if(numRows == 1) return vector<vector<int>>{vector<int>{1}};
+        if(numRows == 2) return vector<vector<int>>{vector<int>{1}, vector<int>{1,1}};
+        vector<vector<int>> res;
+        res.push_back(vector<int>{1});
+        res.push_back(vector<int>{1,1});
+        int cur = 3;
+        while(cur <= numRows)
+        {
+            vector<int> tmp(cur, 1);
+            for(int i = 1; i < cur - 1; i++)
+                tmp[i] = res[cur - 2][i - 1] + res[cur - 2][i];
+            res.push_back(tmp);
+            cur++;
+        }
+        return res;
+    }
+};
