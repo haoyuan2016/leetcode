f779d09f47fbaa80ea54796bb5989d5265935eea
haoyuan2016 <haoyuan265@gmail.com>
2016-10-31 20:50:56 -0700

    Update Q125.cpp

--- Q125.cpp
+++ Q125.cpp
@@ -1,3 +1,4 @@
+// Learn to use isalnum, toupper, tolower.
 class Solution {
 public:
     bool isPalindrome(string s) {
@@ -9,6 +10,32 @@ public:
             while(!(tolower(s[l]) - 'a' <= 26 && tolower(s[l]) - 'a' >=0) && !(tolower(s[l]) - '0' >=0 && '9' - tolower(s[l]) >= 0))
                 l++;
             while(!(tolower(s[r]) - 'a' <= 26 && tolower(s[r]) - 'a' >=0) && !(tolower(s[r]) - '0' >=0 && '9' - tolower(s[r]) >= 0))
+                r--;
+            if(l >= r)
+                return true;
+            else if(tolower(s[l]) == tolower(s[r]))
+            {
+                l++;
+                r--;
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+};
+// See solution with isalmun
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.empty()) return true;
+        int l = 0;
+        int r = s.size() - 1;
+        while(l < r)
+        {
+            while(!isalnum(s[l]))
+                l++;
+            while(!isalnum(s[r]))
                 r--;
             if(l >= r)
                 return true;
