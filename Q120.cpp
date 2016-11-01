90e56b5bce13432c4b27b753712467eaa13622f2
haoyuan2016 <haoyuan265@gmail.com>
2016-10-31 17:37:20 -0700

    Create Q120.cpp

---
+++ Q120.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if(triangle.empty()) return 0;
+        int m = triangle.size();
+        vector<int> dp(m, 0);
+        dp[0] = triangle[0][0];
+        int cur = 1;
+        while(cur < m)
+        {
+            vector<int> tmp = dp;
+            dp[0] = dp[0] + triangle[cur][0];
+            dp[cur] = tmp[cur - 1] + triangle[cur][cur];
+            for(int i = 1; i <= cur - 1; i++)
+                dp[i] = min(tmp[i - 1] + triangle[cur][i], tmp[i] + triangle[cur][i]);
+            cur++;
+        }
+        int res = INT_MAX;
+        for(int a : dp)
+            res = min(res, a);
+        return res;
+    }
+};
