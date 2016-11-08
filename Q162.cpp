7e581db64d7cf2be7a83e6d4f84c9681eded7751
haoyuan2016 <haoyuan265@gmail.com>
2016-11-07 20:54:35 -0800

    Update Q162.cpp

--- Q162.cpp
+++ Q162.cpp
@@ -17,3 +17,21 @@ public:
         return -1;
     }
 };
+
+// Solution with time complexity Q(logN)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while(l < r)
+        {
+            int mid = l + (r - l) / 2; 
+            if(nums[mid] < nums[mid + 1])
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        return l;
+    }
+};
