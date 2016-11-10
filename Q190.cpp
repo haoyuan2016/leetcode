70694b9fe9f08694c6ee66f6b58abebbefdd6dea
haoyuan2016 <haoyuan265@gmail.com>
2016-11-10 15:48:05 -0800

    Create Q190.cpp

---
+++ Q190.cpp
@@ -0,0 +1,13 @@
+// Note that hexidecimal represent four bits, so it is easy taht f is 1111 and 0 is 0000.
+// We use the divide and conquer, the idea is to switch. The key exists in the bit operation & , | and <<. A tricky problem.
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
+};
