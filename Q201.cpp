0283c77855e25b6be597b49ca6194ba413306622
haoyuan2016 <haoyuan265@gmail.com>
2016-11-15 17:12:59 -0800

    Create Q201.cpp

---
+++ Q201.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int tmp = n;
+        int len = 0;
+        int res = 0;
+        while(tmp > 0)
+        {
+            tmp = tmp >> 1;
+            len++;
+        }
+        int dis = n - m;
+        int cur = pow(2, len - 1);
+        if(dis < cur && m / cur % 2 != 0 && n / cur % 2 != 0)
+            res += 1;
+        while(len > 1)
+        {
+            len--;
+            res = res << 1;
+            int cur = pow(2, len - 1);
+            if(dis < cur && m / cur % 2 != 0 && n / cur % 2 != 0)
+                res += 1;
+        }
+        return res;
+    }
+};
