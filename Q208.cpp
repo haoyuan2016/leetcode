744d2a5ed02138b3c9aa6982b0fdee4b3b4b2164
haoyuan2016 <haoyuan265@gmail.com>
2016-11-16 17:46:27 -0800

    Update Q208.cpp

--- Q208.cpp
+++ Q208.cpp
@@ -1,3 +1,6 @@
+// Pay attention to how to initialize an array in class;
+// 1) Trienode* next[26];
+// 2) for (auto &a : next)
 class TrieNode {
 public:
 	// Initialize your data structure here.
@@ -17,7 +20,6 @@ public:
 	}
 	// Inserts a word into the trie.
 	void insert(string word) {
-		int tmp = 1;
 		TrieNode* ptr = root;
 		for (int i = 0; i < word.size(); i++)
 		{
@@ -29,9 +31,8 @@ public:
 				ptr->next[word[i] - 'a'] = tmp;
 				ptr = tmp;
 			}
-			if (i == word.size() - 1)
-				ptr->isWord = true;
 		}
+		ptr->isWord = true;
 	}
 	bool search(string word) {
 		if (word.empty()) return true;
@@ -42,10 +43,9 @@ public:
 				ptr = ptr->next[word[i] - 'a'];
 			else
 				return false;
-			if (i == word.size() - 1)
-				if (ptr->isWord == false)
-					return false;
 		}
+		if (ptr->isWord == false)
+			return false;
 		return true;
 	}
 	bool startsWith(string prefix) {
@@ -56,8 +56,73 @@ public:
 				ptr = ptr->next[prefix[i] - 'a'];
 			else
 				return false;
-			if (i == prefix.size())
-			if (ptr->isWord == true) return false;
+		}
+		return true;
+	}
+private:
+	TrieNode* root;
+};
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie;
+// trie.insert("somestring");
+// trie.search("key");
+
+class TrieNode {
+public:
+	// Initialize your data structure here.
+	TrieNode* next[26];
+	bool isWord;
+	TrieNode() {
+		isWord = false;
+		for (auto &a : next)
+			a = NULL;
+	}
+};
+
+class Trie {
+public:
+	Trie() {
+		root = new TrieNode();
+	}
+	// Inserts a word into the trie.
+	void insert(string word) {
+		TrieNode* ptr = root;
+		for (int i = 0; i < word.size(); i++)
+		{
+			if (ptr->next[word[i] - 'a'])
+				ptr = ptr->next[word[i] - 'a'];
+			else
+			{
+				TrieNode* tmp = new TrieNode();
+				ptr->next[word[i] - 'a'] = tmp;
+				ptr = tmp;
+			}
+		}
+		ptr->isWord = true;
+	}
+	bool search(string word) {
+		if (word.empty()) return true;
+		TrieNode* ptr = root;
+		for (int i = 0; i < word.size(); i++)
+		{
+			if (ptr->next[word[i] - 'a'])
+				ptr = ptr->next[word[i] - 'a'];
+			else
+				return false;
+		}
+		if (ptr->isWord == false)
+			return false;
+		return true;
+	}
+	bool startsWith(string prefix) {
+		TrieNode* ptr = root;
+		for (int i = 0; i < prefix.size(); i++)
+		{
+			if (ptr->next[prefix[i] - 'a'])
+				ptr = ptr->next[prefix[i] - 'a'];
+			else
+				return false;
 		}
 		return true;
 	}
