9b31712637d5b6ac178edfed9b76531e2b4dfea2
haoyuan2016 <haoyuan265@gmail.com>
2016-11-16 20:04:20 -0800

    Update Q210.cpp

--- Q210.cpp
+++ Q210.cpp
@@ -1,3 +1,4 @@
+//Solution with BFS
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
@@ -38,3 +39,35 @@ public:
         return indegree;
     }
 };
+// Solution with DFS
+class Solution {
+public:
+vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+		vector<unordered_set<int>> graph = getGraph(numCourses, prerequisites);
+		vector<bool> visited(numCourses, false), onpath(numCourses, false);
+		vector<int> path;
+		for (int i = 0; i < numCourses; i++)
+		if (!visited[i] && isCirle(visited, onpath, graph, i, path)) return vector<int>{};
+		reverse(path.begin(), path.end());
+		return path;
+	}
+	vector<unordered_set<int>> getGraph(int numCourses, vector<pair<int, int>>& prerequisites)
+	{
+		vector<unordered_set<int>> graph(numCourses);
+		for (int i = 0; i < prerequisites.size(); i++)
+			graph[prerequisites[i].second].insert(prerequisites[i].first);
+		return graph;
+	}
+	bool isCirle(vector<bool>& visited, vector<bool>& onpath, vector<unordered_set<int>>& graph, int i, vector<int>& path)
+	{
+		if (visited[i])  return false;
+		visited[i] = true;
+		onpath[i] = true;
+		for (int a : graph[i])			
+			if (onpath[a] || isCirle(visited, onpath, graph, a, path)) return true;
+		path.push_back(i);
+		onpath[i] = false;
+		return false;
+	}
+};
+
