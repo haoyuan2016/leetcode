2a5fb9df00e131c3f990cc81b5f3eceb440c1a83
haoyuan2016 <haoyuan265@gmail.com>
2016-11-17 15:17:46 -0800

    Update Q215.cpp

--- Q215.cpp
+++ Q215.cpp
@@ -19,3 +19,36 @@ public:
         return -1;
     }
 };
+
+// Also see solution on  https://discuss.leetcode.com/topic/15256/4-c-solutions-using-partition-max-heap-priority_queue-and-multiset-respectively
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while(true)
+        {
+            int pos = getPos(nums, left, right);
+            if(k == pos + 1) return nums[pos];
+            else if(k > pos + 1)
+                left = pos + 1;
+            else
+                right = pos - 1;
+        }
+        return -1;
+    }
+    int getPos(vector<int>& nums, int left, int right)
+    {
+        int l = left + 1; 
+        int r = right;
+        while(l <= r)
+        {
+            if(nums[l] < nums[left] && nums[r] > nums[left])
+                swap(nums[l], nums[r]);
+            if(nums[l] >= nums[left]) l++;
+            if(nums[r] <= nums[left]) r--;
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+};
