9674abe64c128568a5f81dc4a44efd17475177c6
haoyuan2016 <haoyuan265@gmail.com>
2016-11-22 00:24:56 -0800

    Update Q230.cpp

--- Q230.cpp
+++ Q230.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// Solution one
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
@@ -20,5 +21,33 @@ public:
         if(root->left) helper(root->left, res);
             res.push_back(root->val);
         if(root->right) helper(root->right, res);
+    }
+};
+// Solution two
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+    int res;
+    int cur = k;
+    helper(root, cur, res);
+    return res;
+    }
+    void helper(TreeNode* root, int& cur, int& res)
+    {
+        if(root == NULL) return;
+        if(root->left) helper(root->left, cur, res);
+        cur--;
+        if(cur == 0)
+            res = root->val;
+        if(root->right) helper(root->right, cur, res);
     }
 };
