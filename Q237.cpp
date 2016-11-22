5ed810c32369e86771a794475d02c7e40feaa7a7
haoyuan2016 <haoyuan265@gmail.com>
2016-11-22 02:29:07 -0800

    Update Q237.cpp

--- Q237.cpp
+++ Q237.cpp
@@ -13,3 +13,21 @@ public:
         node->next = node->next->next;
     }
 };
+// Or delete the next node
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        auto tmp = node->next;
+        node->val = node->next->val;
+        node->next = node->next->next;
+        delete tmp;
+    }
+};
