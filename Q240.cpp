98dfc13198274bac750555b1b57011a47fba62f8
haoyuan2016 <haoyuan265@gmail.com>
2016-11-22 17:38:19 -0800

    Create Q240.cpp

---
+++ Q240.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty()) return false;
+        int m = matrix.size(); 
+        int n = matrix[0].size();
+        int i = m - 1; 
+        int j = 0;
+        while(i >= 0 && j < n)
+        {
+            if(target == matrix[i][j])
+                return true;
+            else if(target > matrix[i][j])
+                j++;
+            else
+                i--;
+        }
+        return false;
+    }
+};
