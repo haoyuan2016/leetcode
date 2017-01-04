3c5635855d0d6c9aa5ef985e7980c3b2f3cbc6dc
haoyuan2016 <haoyuan265@gmail.com>
2017-01-03 17:52:21 -0800

    Create Q318.cpp

---
+++ Q318.cpp
@@ -0,0 +1,19 @@
+// solution one, time out
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        if(words.empty()) return 0;
+        int res = 0;
+        int n = words.size();
+        vector<int> bitOper(n, 0);
+        for(int i = 0; i < n; i++)
+           for(int j = 0; j < words[i].size(); j++)
+               bitOper[i] = bitOper[i] | (1 << (words[i][j] - 'a'));
+        for(int i = 0; i < words.size() - 1; i++)
+            for(int j = i + 1; j < words.size(); j++)
+                if((bitOper[i] & bitOper[j]) == 0)
+                    res = max(res, (int)words[i].size() * (int)words[j].size());
+        return res;
+    }
+
+};
