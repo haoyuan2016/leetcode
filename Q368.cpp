c4919089160f7556356eee3b893394d5e37f491a
haoyuan2016 <haoyuan265@gmail.com>
2017-02-02 23:16:59 -0800

    Create Q368.cpp

---
+++ Q368.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        if(nums.empty()) return vector<int>();
+        int n = nums.size();
+        if(n == 1) return nums;
+        sort(nums.begin(), nums.end());
+        vector<int> T(n, 1);
+        vector<int> parent(n, 0);
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                int localMax = 0;
+                if(nums[i] % nums[j] == 0 && T[i] < T[j] + 1)
+                {
+                     T[i] = T[j] + 1;
+                     parent[i] = j;
+                }
+            }
+        }
+		vector<int> res;
+		int resMax = 0;
+		int index = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (T[i] > resMax)
+			{
+				resMax = T[i];
+				index = i;
+			}
+		}
+		int tmp = T[index];
+		while (tmp)
+		{
+			res.push_back(nums[index]);
+			index = parent[index];
+			tmp--;
+		}
+		reverse(res.begin(), res.end());
+		return res;
+    }
+};
