b371c6c4941da277b67f5fce6a452280e1bf4eaf
haoyuan2016 <haoyuan265@gmail.com>
2017-02-02 21:09:43 -0800

    Create Q367.cpp

---
+++ Q367.cpp
@@ -0,0 +1,39 @@
+// solution one, use binary search
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long l = 1;
+        long r = num;
+        while(l <= r)
+        {
+            long mid = l + (r - l) / 2;
+            if(mid * mid == num) return true;
+            else if(mid * mid < num)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return false;
+        
+    }
+};
+
+// solution two:
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long l = 1;
+        long r = num;
+        while(l <= r)
+        {
+            long mid = l + (r - l) / 2;
+            if(mid * mid == num) return true;
+            else if(mid * mid < num)
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+        return false;
+        
+    }
+};
