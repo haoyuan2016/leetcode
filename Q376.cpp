772326af0cbe3f44138fe28bf3666dc875c9ccc8
haoyuan2016 <haoyuan265@gmail.com>
2017-02-07 17:06:56 -0800

    Update Q376.cpp

--- Q376.cpp
+++ Q376.cpp
@@ -12,33 +12,22 @@ public:
         {
             int localSign = -2;
             int localMax = INT_MIN;
-            bool doubleLocalMax1 = false;
-            bool doubleLocalMax2 = false;
             for(int j = 0; j <= i - 1; j++)
             {
                 if(nums[i] - nums[j] == 0)
                     continue;
                 else if(nums[i] - nums[j] > 0 && (sign[j] == -1 || sign[j] == 0))
                 {
-				int tmp = localMax;
-				if (tmp == dp[j] + 1) doubleLocalMax2 = true;
-				if (tmp < localMax) doubleLocalMax2 = false;
                     localMax = max(localMax, dp[j] + 1);
                     localSign = 1;
                 }
                 else if(nums[i] - nums[j] < 0 && (sign[j] == 1 || sign[j] == 0))
                 {
-				int tmp = localMax;
-				if (tmp == dp[j] + 1) doubleLocalMax2 = true;
-				if (tmp < localMax) doubleLocalMax2 = false;
                     localMax = max(localMax, dp[j] + 1);
                     localSign = -1;
                 }
                 dp[i] = localMax;
-                if(doubleLocalMax1 && doubleLocalMax2)
-                    sign[i] = 0;
-                else
-                    sign[i] = localSign;
+                sign[i] = localSign;
             }
         }
         return dp[n - 1];
