b039847abf2d663acc3f088110874590cf5c307a
haoyuan2016 <haoyuan265@gmail.com>
2017-02-14 16:06:30 -0800

    Update Q382.cpp

--- Q382.cpp
+++ Q382.cpp
@@ -28,3 +28,42 @@ private:
     int length = 0;
     ListNode* fake_head = NULL;
 };
+// solution two: using probability.
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        ListNode* ptr = head;
+        int cur = 1;
+        int tmp = head->val;
+        while(ptr->next)
+        {
+            ptr = ptr->next;
+            cur++;
+            if(rand() % cur == cur - 1) tmp = ptr->val;
+        }
+        return tmp;
+    }
+private:
+    ListNode* head;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
